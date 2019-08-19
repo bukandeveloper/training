6db1702e4a6ce7a4bf9a0de503529cfb175ee727
fariduan <knight.fm94@gmail.com>
2019-08-19 21:41:42 +0700

    dev

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,8 +2,23 @@
 
 namespace App\Models;
 
-class Member extends \App\Models\Base\Member
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Notifications\Notifiable;
+use App\Notifications\AdminResetPasswordNotification;
+
+class Member extends Model implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
 {
+	use Authenticatable,
+		Authorizable,
+		CanResetPassword,
+		Notifiable;
+
 	protected $hidden = [
 		'password',
 		'remember_token'
